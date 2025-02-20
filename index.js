64c873d3c8061f1e39dce7fbd53420535d256057
Lasma Poksane <laasmapoksane@gmail.com>
2025-02-20 14:21:52 +0100

    Set up Express server with essential middleware

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
