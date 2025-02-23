3b64d303c75a1f91fc9af4b4b8e79991a8690be5
Lasma Poksane <laasmapoksane@gmail.com>
2025-02-23 18:54:45 +0100

    Render routes concerning authorization and registration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth-routes");
 
 const app = express();
 const port = process.env.PORT;
@@ -8,6 +9,8 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
