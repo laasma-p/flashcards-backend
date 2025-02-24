0101e01ebfcb3ec87ba9d950722df54f049d3607
Lasma Poksane <laasmapoksane@gmail.com>
2025-02-25 00:29:04 +0100

    Render routes concerning the user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth-routes");
+const userRoutes = require("./routes/user-routes");
 
 const app = express();
 const port = process.env.PORT;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
