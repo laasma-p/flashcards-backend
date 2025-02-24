cb6b3b727d0873ba30d90c54e2e1886a6ba888ec
Lasma Poksane <laasmapoksane@gmail.com>
2025-02-25 00:37:02 +0100

    Create JWT authentication middleware

---
+++ middleware/jwt.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = verifyToken;
